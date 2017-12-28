610c1793581605cc09b2b883cbf473f35df0db30
DG Kim <donggyunkim@gmail.com>
2017-12-28 01:11:20 -0800

    Probably incorrect implementation of student wavenet, pushing for backup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Code inspired by https://medium.com/@devnag/generative-adversarial-networks-gans-in-50-lines-of-code-pytorch-e81b79659e3f
+# and https://github.com/ibab/tensorflow-wavenet
 
 from __future__ import print_function
 
@@ -16,6 +17,144 @@ import numpy as np
 
 from wavenet import WaveNetModel, AudioReader, optimizer_factory
 
-def get_generator_input_sampler():
-	return lambda mu, sigma, n: np.random.normal(mu, sigma, size=n)
+# GAN params
+num_epochs = 1
 
+# Wavenet params
+BATCH_SIZE = 1
+DATA_DIRECTORY = './VCTK-Corpus'
+LOGDIR_ROOT = './logdir'
+CHECKPOINT_EVERY = 50
+NUM_STEPS = int(1e5)
+LEARNING_RATE = 1e-3
+WAVENET_PARAMS = './wavenet_params.json'
+STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
+SAMPLE_SIZE = 100000
+L2_REGULARIZATION_STRENGTH = 0
+SILENCE_THRESHOLD = 0.3
+EPSILON = 0.001
+MOMENTUM = 0.9
+MAX_TO_KEEP = 5
+METADATA = False
+
+def get_arguments():
+    def _str_to_bool(s):
+        """Convert string to bool (in argparse context)."""
+        if s.lower() not in ['true', 'false']:
+            raise ValueError('Argument needs to be a '
+                             'boolean, got {}'.format(s))
+        return {'true': True, 'false': False}[s.lower()]
+
+    parser = argparse.ArgumentParser(description='WaveNet example network')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
+                        help='How many wav files to process at once. Default: ' + str(BATCH_SIZE) + '.')
+    parser.add_argument('--data_dir', type=str, default=DATA_DIRECTORY,
+                        help='The directory containing the VCTK corpus.')
+    parser.add_argument('--store_metadata', type=bool, default=METADATA,
+                        help='Whether to store advanced debugging information '
+                        '(execution time, memory consumption) for use with '
+                        'TensorBoard. Default: ' + str(METADATA) + '.')
+    parser.add_argument('--logdir', type=str, default=None,
+                        help='Directory in which to store the logging '
+                        'information for TensorBoard. '
+                        'If the model already exists, it will restore '
+                        'the state and will continue training. '
+                        'Cannot use with --logdir_root and --restore_from.')
+    parser.add_argument('--logdir_root', type=str, default=None,
+                        help='Root directory to place the logging '
+                        'output and generated model. These are stored '
+                        'under the dated subdirectory of --logdir_root. '
+                        'Cannot use with --logdir.')
+    parser.add_argument('--restore_from', type=str, default=None,
+                        help='Directory in which to restore the model from. '
+                        'This creates the new model under the dated directory '
+                        'in --logdir_root. '
+                        'Cannot use with --logdir.')
+    parser.add_argument('--checkpoint_every', type=int,
+                        default=CHECKPOINT_EVERY,
+                        help='How many steps to save each checkpoint after. Default: ' + str(CHECKPOINT_EVERY) + '.')
+    parser.add_argument('--num_steps', type=int, default=NUM_STEPS,
+                        help='Number of training steps. Default: ' + str(NUM_STEPS) + '.')
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE,
+                        help='Learning rate for training. Default: ' + str(LEARNING_RATE) + '.')
+    parser.add_argument('--wavenet_params', type=str, default=WAVENET_PARAMS,
+                        help='JSON file with the network parameters. Default: ' + WAVENET_PARAMS + '.')
+    parser.add_argument('--sample_size', type=int, default=SAMPLE_SIZE,
+                        help='Concatenate and cut audio samples to this many '
+                        'samples. Default: ' + str(SAMPLE_SIZE) + '.')
+    parser.add_argument('--l2_regularization_strength', type=float,
+                        default=L2_REGULARIZATION_STRENGTH,
+                        help='Coefficient in the L2 regularization. '
+                        'Default: False')
+    parser.add_argument('--silence_threshold', type=float,
+                        default=SILENCE_THRESHOLD,
+                        help='Volume threshold below which to trim the start '
+                        'and the end from the training set samples. Default: ' + str(SILENCE_THRESHOLD) + '.')
+    parser.add_argument('--optimizer', type=str, default='adam',
+                        choices=optimizer_factory.keys(),
+                        help='Select the optimizer specified by this option. Default: adam.')
+    parser.add_argument('--momentum', type=float,
+                        default=MOMENTUM, help='Specify the momentum to be '
+                        'used by sgd or rmsprop optimizer. Ignored by the '
+                        'adam optimizer. Default: ' + str(MOMENTUM) + '.')
+    parser.add_argument('--histograms', type=_str_to_bool, default=False,
+                        help='Whether to store histogram summaries. Default: False')
+    parser.add_argument('--gc_channels', type=int, default=None,
+                        help='Number of global condition channels. Default: None. Expecting: Int')
+    parser.add_argument('--max_checkpoints', type=int, default=MAX_TO_KEEP,
+                        help='Maximum amount of checkpoints that will be kept alive. Default: '
+                             + str(MAX_TO_KEEP) + '.')
+    return parser.parse_args()
+
+def get_generator_input_sampler():
+    return lambda mu, sigma, n: np.random.normal(mu, sigma, size=n)
+
+def main():
+    # Generator params
+    white_mean = 0
+    white_sigma = 1
+    white_length = 44100
+
+    gi_sampler = get_generator_input_sampler()
+
+    args = get_arguments()
+
+    with open(args.wavenet_params, 'r') as f:
+        wavenet_params = json.load(f)
+
+    G = WaveNetModel(
+        batch_size=args.batch_size,
+        dilations=wavenet_params["dilations"],
+        filter_width=wavenet_params["filter_width"],
+        residual_channels=wavenet_params["residual_channels"],
+        dilation_channels=wavenet_params["dilation_channels"],
+        skip_channels=wavenet_params["skip_channels"],
+        quantization_channels=wavenet_params["quantization_channels"],
+        use_biases=wavenet_params["use_biases"],
+        scalar_input=wavenet_params["scalar_input"],
+        initial_filter_width=wavenet_params["initial_filter_width"],
+        histograms=args.histograms)
+    
+    # Set up session
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        gen_input = tf.convert_to_tensor(gi_sampler(white_mean, white_sigma, white_length), np.float32)
+        loss = G.loss(input_batch=gen_input)
+
+        optimizer = optimizer_factory[args.optimizer](
+                    learning_rate=args.learning_rate,
+                    momentum=args.momentum)
+        trainable = tf.trainable_variables()
+        optim = optimizer.minimize(loss, var_list=trainable)
+
+        loss_value, _ = sess.run([loss, optim])
+
+        duration = time.time() - start_time
+        print('step {:d} - loss = {:.3f}, ({:.3f} sec/step)'.format(epoch, loss_value, duration))
+
+if __name__ == '__main__':
+    main()
